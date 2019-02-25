cd70a8cb10a78c09f655e1d7c6024c4a639a92f6
Elias Ferguson <eferguson00@stuy.edu>
2019-02-24 22:39:41 -0500

    now going to attempt to approach optimization

--- Driver.java
+++ Driver.java
@@ -2,5 +2,6 @@ public class Driver {
   public static void main(String[] args) {
     KnightBoard test = new KnightBoard(5, 5);
     System.out.println(test.toString());
+    System.out.println(test.countSolutions(0, 0));
   }
 }
