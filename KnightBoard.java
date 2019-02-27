3c47064dd3198126eb2fd8c269ebfa3f2574792e
Elias Ferguson <eferguson00@stuy.edu>
2019-02-26 23:01:50 -0500

    forgot to add in sort, i did, but still am not getting the correct answer

--- KnightBoard.java
+++ KnightBoard.java
@@ -141,7 +141,7 @@ public class KnightBoard {
       return (r >= 0 && r < rows && c >= 0 && c < cols);
     }
     private boolean solveOptim(Node curr, int moveNum) {
-      if (moveNum > n) return true;
+      if (moveNum == n + 1) return true;
       ArrayList<Node> movesToDo = new ArrayList<Node>();
       for (Node test:optimizedOptions) {
         Node possibleNew = curr.move(test);
@@ -149,6 +149,7 @@ public class KnightBoard {
           movesToDo.add(possibleNew);
         }
       }
+      Collections.sort(movesToDo);
       for (Node option:movesToDo) {
         board[option.row][option.col] = moveNum;
         for (Node change:optimizedOptions) {
